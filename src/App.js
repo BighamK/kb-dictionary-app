4a8d595eaab23c526dc5e0409b988e8cb6627407
BighamK <100013654+BighamK@users.noreply.github.com>
2022-06-30 22:35:31 -0700

    Start a form/search

--- src/App.js
+++ src/App.js
@@ -6,6 +6,22 @@ function App() {
       <header className="App-header">
        <p>Hello</p>
       </header>
+    
+      <form>
+                <div className="row">
+                    <div className="col-9">
+                        <input
+                        type="search"
+                        placeholder="Type in your word..."
+                        className="form-control"
+                        autoFocus="on"
+                        />
+                    </div>
+                    <div className="col-3">
+                        <input type="submit" value="Search" className="btn btn-primary" />
+                    </div>
+                </div>
+            </form>
     </div>
   );
 }
