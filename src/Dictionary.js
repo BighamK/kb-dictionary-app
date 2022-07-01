b8d96b7b758c2823e0043000efc1a3738868849e
BighamK <100013654+BighamK@users.noreply.github.com>
2022-07-01 13:40:40 -0700

    Search form setup

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,9 +2,14 @@ import React from "react";
 import "./Dictionary.css";
 
 export default function Dictionary () {
+    function search(event) {
+        event.preventDefault();
+        alert("Searching...");
+    }
+
     return (
-    <form>
-        
+<div className="Dictionary">
+ <form onSubmit={search}>    
     <div className="row">
         <div className="col-9">
             <input
@@ -18,8 +23,7 @@ export default function Dictionary () {
             <input type="submit" value="Search" className="btn btn-primary" />
         </div>
     </div>
-
 </form>
+</div>
     );
-
 }
