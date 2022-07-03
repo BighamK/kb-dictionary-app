0923677bb3fabfb0c7e99aaad807cae202faa1b5
BighamK <100013654+BighamK@users.noreply.github.com>
2022-07-02 17:44:37 -0700

    Now update the returned definition...

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 export default function Meaning(props) {
-    return "MEEANing";
-
+    return (
+        <div className="Meaning">
+        <h3>{props.meaning.partsOfSpeech}</h3>
+        <p>{props.meaning.definitions[0].definition}</p>
+        <p>{props.meaning.definitions[0].example}</p>
+        </div>
+    );
 }
