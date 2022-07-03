3d2c85956978e5bce93dcdcc0ae726b57ac50b14
BighamK <100013654+BighamK@users.noreply.github.com>
2022-07-02 18:19:27 -0700

    Fixed a typo

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -9,16 +9,16 @@ export default function Dictionary () {
 
 
     function handleResponse(response) {
-        console.log(response.data[0]);
         setResults(response.data[0]);
     }
 
     function search(event) {
         event.preventDefault();
-    }
 
 let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-axios.get(apiUrl).then(handleResponse)
+axios.get(apiUrl).then(handleResponse);
+    }
+
 
     function handleKeywordChange(event) {
         setKeyword(event.target.value);   
