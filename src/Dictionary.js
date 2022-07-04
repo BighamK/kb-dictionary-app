4f93253fa30571c300aadadadc3b279a2c9cd2b9
BighamK <100013654+BighamK@users.noreply.github.com>
2022-07-04 12:46:48 -0700

    Start styling css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,31 +6,50 @@ import "./Dictionary.css";
 export default function Dictionary () {
     let [keyword, setKeyword] = useState("");
     let [results, setResults] = useState(null);
+    let [loaded, setLoaded] = useState(false);
 
 
     function handleResponse(response) {
         setResults(response.data[0]);
     }
 
-    function search(event) {
-        event.preventDefault();
+    function search() {
 
 let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 axios.get(apiUrl).then(handleResponse);
     }
 
 
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
+    }
+
     function handleKeywordChange(event) {
         setKeyword(event.target.value);   
     }
 
+    function load() {
+        setLoaded(true);
+        search();
+    }
+
+    if (loaded) {
     return (
          <div className="Dictionary">
-
-                 <form onSubmit={search}>   
+<section>
+                 <form onSubmit={handleSubmit}>   
                    <input type="search" onChange={handleKeywordChange} />
                 </form>
+                <div className="hint">
+                    Suggested words: sunset, dog, happy...
+                </div>
+                </section>
                 <Results results={results} />
            </div>
     );
+} else {
+    load();
+    return "Searching";
+}
 }
