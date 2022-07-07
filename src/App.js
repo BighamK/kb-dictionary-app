84b6e8fa6722f3d2339f6550194861cf39161d96
BighamK <100013654+BighamK@users.noreply.github.com>
2022-07-06 21:00:57 -0700

    Bold header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <div className="Header text-center">
-        Dictionary App <br /><small>Vocabulary at your fingertips</small>
+        <strong>Dictionary App</strong> <br /><small>Vocabulary at your fingertips</small>
       </div>
 
     <main>
