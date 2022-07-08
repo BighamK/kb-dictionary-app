277fc8e31ac708eec78e3c177f69b4dc34a46b5b
BighamK <100013654+BighamK@users.noreply.github.com>
2022-07-07 17:05:34 -0700

    Eliminate extra audio?
    Trying it out...

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ export default function Phonetic(props)  {
 
 return (
         <div className="Phonetic">
-            <a href={props.phonetic.audio} >
+            <a href={props.phonetic.audio ? props.phonetic.audio : '#'} >
             Listen
             </a>
         <span className="sounds">
